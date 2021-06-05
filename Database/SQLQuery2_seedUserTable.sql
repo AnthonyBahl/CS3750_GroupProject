b75136b55ab82b9c1b529290399c060746d6589c
KylerHansen <kylerhansen@mail.weber.edu>
2021-06-04 18:43:37 -0600

    removed the .dll

---
+++ Database/SQLQuery2_seedUserTable.sql
@@ -0,0 +1,30 @@
+USE [cs3750]
+GO
+
+/****** Object:  Table [dbo].[Users]    Script Date: 6/2/2021 9:05:32 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Users](
+	[userID] [int] IDENTITY(1,1) NOT NULL,
+	[email] [varchar](100) NOT NULL,
+	[firstName] [varchar](100) NOT NULL,
+	[lastName] [varchar](100) NOT NULL,
+	[birthday] [date] NOT NULL,
+	[password] [varchar](256) NOT NULL,
+	[accountType] [smallint] NOT NULL,
+	[address1] [varchar](100) NULL,
+	[address2] [varchar](100) NULL,
+	[city] [varchar](100) NULL,
+	[state] [int] NULL,
+	[zip] [varchar](30) NULL,
+	[phone] [varchar](30) NULL,
+ CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED 
+(
+	[userID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
