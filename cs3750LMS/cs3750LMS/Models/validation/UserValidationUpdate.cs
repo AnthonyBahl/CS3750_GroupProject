676a59737d5eb38c4e2c507023419f3128c67568
GiancarloGutierrez <giancarlo0015@gmail.com>
2021-06-08 23:48:06 -0600

    States dropdown

--- cs3750LMS/cs3750LMS/Models/validation/UserValidationUpdate.cs
+++ cs3750LMS/cs3750LMS/Models/validation/UserValidationUpdate.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Drawing;
+using cs3750LMS.Models.validation;
 
 namespace cs3750LMS.Models
 {
