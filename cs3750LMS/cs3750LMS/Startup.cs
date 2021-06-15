b72a608cbdbad7b2ac636718aa8074c952ea845c
Henry Doan <henrydoan@mail.weber.edu>
2021-06-14 23:55:04 -0600

    started to add stripe stuff

--- cs3750LMS/cs3750LMS/Startup.cs
+++ cs3750LMS/cs3750LMS/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Stripe;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -48,6 +49,7 @@ namespace cs3750LMS
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            StripeConfiguration.ApiKey = "sk_test_51J1K0xA6qDyGoLeeC6aj7Rm39c8lFFfTYZ9k4KyAy6oxH30YYJEKbE73mewvQAAc0jkzATCmsOuzwZ7pZ42bXBSc00t8sYSK1X";
 
             if (env.IsDevelopment())
             {
