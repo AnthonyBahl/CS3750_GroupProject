45cc9b43e152d830543470357090bf9c1e0b6f2a
Henry Doan <henrydoan@mail.weber.edu>
2021-06-16 22:38:06 -0600

    remove stipe package and content and refs

--- cs3750LMS/cs3750LMS/Controllers/StudentController.cs
+++ cs3750LMS/cs3750LMS/Controllers/StudentController.cs
@@ -7,8 +7,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
-using Stripe;
 
 namespace cs3750LMS.Controllers
 {
@@ -319,37 +317,6 @@ namespace cs3750LMS.Controllers
                 }
             }
             return View("~/Views/Home/Login.cshtml");
-        }
-
-        [HttpPost]
-        public ActionResult Create(PaymentIntentCreateRequest request)
-        {
-            var paymentIntents = new PaymentIntentService();
-            var paymentIntent = paymentIntents.Create(new PaymentIntentCreateOptions
-            {
-                Amount = CalculateOrderAmount(request.Items),
-                Currency = "usd",
-            });
-            return Json(new { clientSecret = paymentIntent.ClientSecret });
-        }
-
-        private int CalculateOrderAmount(Item[] items)
-        {
-            // Replace this constant with a calculation of the order's amount
-            // Calculate the order total on the server to prevent
-            // people from directly manipulating the amount on the client
-            return 1400;
-        }
-        public class Item
-        {
-            [JsonProperty("id")]
-            public string Id { get; set; }
-        }
-
-        public class PaymentIntentCreateRequest
-        {
-            [JsonProperty("items")]
-            public Item[] Items { get; set; }
         }
     }
 }
