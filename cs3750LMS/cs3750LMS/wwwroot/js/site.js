01e0688f9e31bc1ee1c29ac92040f7f029832192
KylerHansen <kylerhansen@mail.weber.edu>
2021-06-21 17:04:42 -0600

    basic layout done working on animation javascript.

--- cs3750LMS/cs3750LMS/wwwroot/js/site.js
+++ cs3750LMS/cs3750LMS/wwwroot/js/site.js
@@ -1,4 +1,25 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
 
-// Write your JavaScript code.
+
+//Notification Bell In Navbar
+//-------------------------------------------
+const bellIcon = document.getElementById("bellIcon");
+
+let ItemsCount = 3; // we need to get the amount of items passed into the notifications list and store it here. 
+
+if (ItemsCount > 0) {
+
+        //bellIcon.classList.add("") //add animation to bell here. 
+
+        bellIcon.style.color = "#D94B2B"; // set color to orange
+
+    } else {
+        bellIcon.style.color = "#495057";  //set color to nav-link gray. 
+    }
+
+function removeNotificationItem() {
+   // document.getElementById("");
+}
+
+
+//END --------------  Notification Bell In Navbar
